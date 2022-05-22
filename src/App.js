d6b44fae6e533b2ce884f9799be0318a044b7461
IftekharAhmedS <ahamedsantonu@gmail.com>
2022-05-22 21:45:42 +0600

    added login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const Header = () => {
-  
   const [user, loading, error] = useAuthState(auth);
 
+  const logout = () => {
+    signOut(auth);
+  };
 
   return (
     <div className="navbar bg-base-100 container mx-auto">
@@ -78,12 +81,36 @@ const Header = () => {
       </div>
       <div className="navbar-end">
         <ul className="menu menu-horizontal p-0">
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li tabIndex="0">
-            <Link to="/">Login</Link>
-          </li>
+          {user ? (
+            <>
+              <button className="btn gap-2" onClick={logout}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                  />
+                </svg>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li tabIndex="0">
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
